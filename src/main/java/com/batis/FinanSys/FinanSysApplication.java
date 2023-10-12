90b013b6c1634e45e0c7efbc86a4706bb6d91cbb
Samuel <samuel.batistela12@gmail.com>
2023-10-12 12:23:59 -0300

    first commit

---
+++ src/main/java/com/batis/FinanSys/FinanSysApplication.java
@@ -0,0 +1,13 @@
+package com.batis.FinanSys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanSysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanSysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/batis/FinanSys/FinanSysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.batis.FinanSys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanSysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
